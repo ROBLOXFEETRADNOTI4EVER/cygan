bd711cd9720b6fff68f7a92a54a2cccc06331e5d
THEFFEETYMIGHTYMOO <108127820+ROBLOXFEETRADNOTI4EVER@users.noreply.github.com>
2026-02-07 19:51:08 +0100

    Refactor math operations to use fsize type

--- main.rs
+++ main.rs
@@ -2,7 +2,7 @@ use regex::Regex;
 use std::fs::File;
 use std::io::ErrorKind;
 use std::io::Read;
-
+use fsize::fsize;
 fn main() {
     let cz_file_results = File::open("main.cz");
 
@@ -37,28 +37,96 @@ fn main() {
 fn check_syntax(file_read: &str) -> String {
     // later we will require main fn
     // need to check if its ascii only no wierd symbols later on handle it to
-    file_read.replace(" ", "\n")
+    // Here a logic thing that deletes lines that have // since its commenting out
+    // file_read.replace(" ", "\n")
+    file_read.replace(" ", "\n");
+    let mut new_vec: Vec<char> = Vec::new();
+    let mut found_shit = false;
+    for mut i in 0..file_read.len(){
+    
+        if file_read.chars().nth(i).unwrap() == '/' && file_read.chars().nth(i + 1).unwrap() == '/'{
+             found_shit = true;
+
+        }
+        if file_read.chars().nth(i).unwrap() == '\n'{
+            found_shit = false;
+            continue;
+        }
+
+        if !found_shit{
+            let  a: char = file_read.chars().nth(i).unwrap();
+            new_vec.push(a);
+        }
+    }
+    // println!("vec is {:#?}",new_vec);
+    let result: String = new_vec.iter().collect();
+    // println!("result is {:#?}",result);
+
+    result
 }
 
-fn math_operations_version_0(file_read: &str) -> isize {
+fn math_operations_version_0(file_read: &str) -> fsize {
+    // need to check for line by line
     for i in 0..file_read.len() - 1 {
         file_read.chars().nth(i).unwrap();
     }
 
-    let re = Regex::new(r"^\s*add\(\s*(-?\d+)\s*,\s*(-?\d+)\s*\)\s*;\s*$").unwrap();
-    if re.is_match(&file_read.replace(" ", "")) {
+    let re_add: Regex = Regex::new(r"^\s*add\(\s*(-?\d+)\s*,\s*(-?\d+)\s*\)\s*;\s*$").unwrap();
+    let re_remove: Regex = Regex::new(r"^\s*remove\(\s*(-?\d+)\s*,\s*(-?\d+)\s*\)\s*;\s*$").unwrap();
+    let re_multiply: Regex = Regex::new(r"^\s*multiply\(\s*(-?\d+)\s*,\s*(-?\d+)\s*\)\s*;\s*$").unwrap();
+    let re_devide: Regex = Regex::new(r"^\s*devide\(\s*(-?\d+)\s*,\s*(-?\d+)\s*\)\s*;\s*$").unwrap();
+
+    if re_add.is_match(&file_read.replace(" ", "")) {
         // println!("ok");
 
-        if let Some(caps) = re.captures(&file_read) {
+        if let Some(caps) = re_add.captures(&file_read) {
             let num1 = caps.get(1).unwrap().as_str();
             let num2 = caps.get(2).unwrap().as_str();
             // println!("First: {}, Second: {}", num1, num2);
             // println!("First num + secs num is ")
-            return num1.parse::<isize>().unwrap() + num2.parse::<isize>().unwrap();
+            return num1.parse::<fsize>().unwrap() + num2.parse::<fsize>().unwrap();
         } else {
             panic!("FAILED TO DO MY JOB");
         }
-    } else {
+    } 
+    else if re_remove.is_match(&file_read.replace(" ","")){
+
+        if let Some(caps) = re_remove.captures(&file_read) {
+            let num1 = caps.get(1).unwrap().as_str();
+            let num2 = caps.get(2).unwrap().as_str();
+            // println!("First: {}, Second: {}", num1, num2);
+            // println!("First num + secs num is ")
+            return num1.parse::<fsize>().unwrap() - num2.parse::<fsize>().unwrap();
+        } else {
+            panic!("FAILED TO DO MY JOB");
+        }
+    }
+
+    else if re_multiply.is_match(&file_read.replace(" ","")){
+
+        if let Some(caps) = re_multiply.captures(&file_read) {
+            let num1 = caps.get(1).unwrap().as_str();
+            let num2 = caps.get(2).unwrap().as_str();
+            // println!("First: {}, Second: {}", num1, num2);
+            // println!("First num + secs num is ")
+            return num1.parse::<fsize>().unwrap() * num2.parse::<fsize>().unwrap();
+        } else {
+            panic!("FAILED TO DO MY JOB");
+        }
+    }
+    else if re_devide.is_match(&file_read.replace(" ","")){
+
+        if let Some(caps) = re_devide.captures(&file_read) {
+            let num1 = caps.get(1).unwrap().as_str();
+            let num2 = caps.get(2).unwrap().as_str();
+            // println!("First: {}, Second: {}", num1, num2);
+            // println!("First num + secs num is ")
+            return num1.parse::<fsize>().unwrap() / num2.parse::<fsize>().unwrap();
+        } else {
+            panic!("FAILED TO DO MY JOB");
+        }
+    }
+    else {
         panic!("FAILED TO DO MY JOB");
     }
 }
